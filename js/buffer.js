ada23f82208b7434126ef6048a82749109f0e21f
Maxime ROUSSEL <maxime.roussel2024@campus-eni.fr>
2024-11-19 16:10:08 +0100

    Avancement dans la logique du jeu

--- js/buffer.js
+++ js/buffer.js
@@ -1,63 +1,88 @@
-window.onload = init;
+//-- création du tableau
+let array = [];
+let x = 6;
+let baliseHtmlCard = [];
 
-function init() {
-  //--Déclaration de mes variables ou constantes
-  const cards = document.querySelectorAll(".card");
-  let bufferCard;
-  let table = [];
-  let newTable = [];
-  let counter = 0
+//-- Rempissage du tableau dans l'ordre
+for(let i=1; i<x ; i++){
+    array.push(i) ;  
+    array.push(i); 
+}
 
-  //--Gestion des événements
-  cards.forEach((card) => {
-    card.addEventListener("click", (event) => {
-      getCardOnclick(event);
+//-- on mélange autant de fois qu'il y a de cartes
+for(let i=0; i<x ; i++){
+  array = shuffle(array);
+}
+console.log(array);
 
-      if(counter<2)
+//--fonction pour mélanger le tableau
+function shuffle(array){
+  const resultArr= [];
 
-      //--Remplir le tableau une seule fois si nécessaire
-      if (table.length === 0) {
-        table = generateRandomCards(1, 13);
-        for (let i = 0; i < table.length / 2; i++) {
-          newTable[i] = table[i];
-          newTable[i + table.length / 2] = table[i];
-        }
-        
-        console.log(newTable);
-      }
-      //-- Animation de la carte
-      bufferCard.style.transform = "rotateY(180deg)";
-      bufferCard.style.transition = "transform 1s"
-
-      //--Obtenir une valeur aléatoire à partir du tableau
-      const randomIndex = Math.floor(Math.random() * newTable.length);
-      const randomValue = newTable[randomIndex];
-
-      //--Exemple d'utilisation (changer l'image source)
-      bufferCard.src = `./sources/dinosaures/${randomValue}.jpg`;
-      const indexRV = newTable.indexOf(randomValue);
-      newTable.splice(indexRV, 1);
-      console.log(newTable);
-
-    });
-  });
-
-  //--Récuperation de la carte cliquée
-  function getCardOnclick(event) {
-    const clickedElement = event.target;
-    bufferCard = clickedElement;
-    const idList = clickedElement.id;
-  }
-
-  //--Générer un tableau de valeurs aléatoires uniques entre min et max
-  function generateRandomCards(min, max) {
-    const array = [];
-    while (array.length < max - min) {
-      const randomNumber = Math.floor(Math.random() * (max - min) + min);
-      if (!array.includes(randomNumber)) {
-        array.push(randomNumber);
+  for(let i=0; i<x; i++){
+    const numAlea = getRandomInt(x);
+      if(numAlea>x/2){
+        resultArr.unshift(array[i]);
+      }else{
+        resultArr.push(array[i]);
       }
     }
-    return array;
+    return resultArr
+}
+
+//-- fonction pour avoir un nombre aléatoire
+function getRandomInt(max){
+  return Math.floor(Math.random()*max)
+}
+//--assignation des nombres aux cartes et affichage selon le nombre de cartes tot (x)
+const gameGrid = document.querySelector(".gameGrid")
+for(let i = 0; i<x ; i++){
+  const element = document.createElement("img");
+  element.classList.add("card");
+  element.id="card"+i;
+  element.src="./sources/dinosaures/"+array[i]+".jpg";
+  gameGrid.appendChild(element);
+  baliseHtmlCard[i]= element;
+ }
+//--jeu
+ 
+let buffer = [0] ;//-- compteur du nombre de cartes sélectionnées
+let cardsOut = [];
+//-- Création d'un addeventlistener sur chaque élément qui possede la classe card
+let cards = document.getElementsByClassName("card");
+cards = [...cards];
+cards.forEach((a) => {
+  a.addEventListener("click", (event) => {
+    launchTirage(event, buffer[0]);
+  });
+});
+//-- créé la condition avant de check les sources 
+function checkCards(cardsOut){
+  if(cardsOut.length <2){
+  }else if(cardsOut.length === 2){
+    compare();
+    cardsOut.length=0;
+  }
+}
+
+function launchTirage(event, buffer){
+  if(buffer === 0){
+    const selectCard = event.target;
+    cardsOut.push(selectCard);
+    checkCards(cardsOut);
+    buffer ++ ;
+  }else if(buffer === 1){
+    const selectCard = event.target;
+    cardsOut.push(selectCard);
+    checkCards(cardsOut);
+    buffer = 0;
+  }
+}
+
+function compare(){
+  if(cardsOut[0].src === cardsOut[1].src){
+    console.log("YOU WIN");
+  }else{
+    console.log("essaye encore");
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,74 +1,86 @@
-window.onload = init;
+//-- création du tableau
+let array = [];
+let x = 6;
+let baliseHtmlCard = [];
 
-function init() {
-  //--Déclaration de mes variables ou constantes
-  const cards = document.querySelectorAll(".card");
-  let bufferCard;
-  let table = [];
-  let newTable = [];
-  let counter = 0;
+//-- Rempissage du tableau dans l'ordre
+for(let i=1; i<x ; i++){
+    array.push(i) ;  
+    array.push(i); 
+}
 
-  //--Gestion des événements
-  cards.forEach((card) => {
-    card.addEventListener("click", (event) => {
-      getCardOnclick(event);
+//-- on mélange autant de fois qu'il y a de cartes
+for(let i=0; i<x ; i++){
+  array = shuffle(array);
+}
+console.log(array);
 
-      if (counter !== 2) {
-        //--Remplir le tableau une seule fois si nécessaire
-        if (table.length === 0) {
-          table = generateRandomCards(1, 13);
-          for (let i = 0; i < table.length / 2; i++) {
-            newTable[i] = table[i];
-            newTable[i + table.length / 2] = table[i];
-          }
+//--fonction pour mélanger le tableau
+function shuffle(array){
+  const resultArr= [];
 
-          console.log(newTable);
-        }
-        //-- Animation de la carte
-        bufferCard.style.transform = "rotateY(180deg)";
-        bufferCard.style.transition = "transform 1s";
-
-        //--Obtenir une valeur aléatoire à partir du tableau
-        const randomIndex = Math.floor(Math.random() * newTable.length);
-        const randomValue = newTable[randomIndex];
-
-        //--Exemple d'utilisation (changer l'image source)
-        bufferCard.src = `./sources/dinosaures/${randomValue}.jpg`;
-        const indexRV = newTable.indexOf(randomValue);
-        newTable.splice(indexRV, 1);
-        console.log(newTable);
-        counter++;
-        console.log(counter);
-      }else if(counter == 2){
-        counter ===0;
-        cards.forEach((card)=>{
-            if(card.src.includes(".jpg")){
-                card.style.transform = "rotateY(180deg)";
-                card.style.transition = "transform 1s"
-            }
-
-            
-        })
-      }
-    });
-  });
-
-  //--Récuperation de la carte cliquée
-  function getCardOnclick(event) {
-    const clickedElement = event.target;
-    bufferCard = clickedElement;
-    const idList = clickedElement.id;
-  }
-
-  //--Générer un tableau de valeurs aléatoires uniques entre min et max
-  function generateRandomCards(min, max) {
-    const array = [];
-    while (array.length < max - min) {
-      const randomNumber = Math.floor(Math.random() * (max - min) + min);
-      if (!array.includes(randomNumber)) {
-        array.push(randomNumber);
+  for(let i=0; i<x; i++){
+    const numAlea = getRandomInt(x);
+      if(numAlea>x/2){
+        resultArr.unshift(array[i]);
+      }else{
+        resultArr.push(array[i]);
       }
     }
-    return array;
+    return resultArr
+}
+
+//-- fonction pour avoir un nombre aléatoire
+function getRandomInt(max){
+  return Math.floor(Math.random()*max)
+}
+//--assignation des nombres aux cartes et affichage selon le nombre de cartes tot (x)
+const gameGrid = document.querySelector(".gameGrid")
+for(let i = 0; i<x ; i++){
+  const element = document.createElement("img");
+  element.classList.add("card");
+  element.id="card"+i;
+  element.src="./sources/dinosaures/"+array[i]+".jpg";
+  gameGrid.appendChild(element);
+  baliseHtmlCard[i]= element;
+ }
+//--jeu
+ 
+let buffer = [0] ;//-- compteur du nombre de cartes sélectionnées
+let cardsOut = [];
+//-- Création d'un addeventlistener sur chaque élément qui possede la classe card
+let cards = document.getElementsByClassName("card");
+cards = [...cards];
+cards.forEach((a) => {
+  a.addEventListener("click", launchTirage)});
+
+//-- créé la condition avant de check les sources 
+function checkCards(cardsOut){
+  if(cardsOut.length <2){
+  }else if(cardsOut.length === 2){
+    compare();
+    cardsOut.length=0;
+  }
+}
+
+function launchTirage(event){
+  if(buffer[0] === 0){
+    const selectCard = event.target;
+    cardsOut.push(selectCard);
+    checkCards(cardsOut);
+    buffer[0] ++ ;
+  }else if(buffer[0] === 1){
+    const selectCard = event.target;
+    cardsOut.push(selectCard);
+    checkCards(cardsOut);
+    buffer[0] = 0;
+  }
+}
+
+function compare(){
+  if(cardsOut[0].src === cardsOut[1].src){
+    console.log("YOU WIN");
+  }else{
+    console.log("essaye encore");
   }
 }
